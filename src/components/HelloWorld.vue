9808d688d78d9b8aabfc17bd68729609a97aee94
David Milan <djmilan1@gmail.com>
2019-05-23 21:31:21 -0400

    feat: 🎸 setup boilerplate with weather.gov api interaction

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{ currentTemp }}</h1>
+    <div v-for="forecast in dailyForecasts" :key="forecast.name">
+      <p>{{ forecast.name }} : {{ forecast.temperature }}</p>
+      <img :src="forecast.icon" style="border-radius: 5px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { setInterval } from "timers";
+
+const forecastURL = "https://api.weather.gov/gridpoints/BOX/66,74/forecast";
+const forecastHourlyURL =
+  "https://api.weather.gov/gridpoints/BOX/66,74/forecast/hourly";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      currentTemp: 0,
+      dailyForecasts: []
+    };
+  },
+  created() {
+    axios.get(forecastHourlyURL).then(response => {
+      this.currentTemp = response.data.properties.periods[0].temperature;
+    });
+    setInterval(() => {
+      axios.get(forecastHourlyURL).then(response => {
+        this.currentTemp = response.data.properties.periods[0].temperature;
+      });
+    }, 3600000);
+    axios.get(forecastURL).then(response => {
+      this.dailyForecasts = response.data.properties.periods;
+    });
+    setInterval(() => {
+      axios.get(forecastURL).then(response => {
+        this.dailyForecasts = response.data.properties.periods;
+      });
+    }, 1200000);
+  }
+};
+</script>
