bc9638782478dbd4565ffd003adf50c25e9eb933
David Milan <djmilan1@gmail.com>
2019-05-25 14:32:38 -0400

    feat: 🎸 parse daily API results and display weathericons

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Forecast from "./components/Forecast.vue";
+import "./assets/css/weathericons.min.css";
 
 export default {
   name: "app",
--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <h1>{{ currentTemp }}</h1>
-    <p>{{ detailedForecast }}</p>
-    <div style="display: inline-block; margin: 5px; width: 100px" v-for="forecast in dailyForecasts" :key="forecast.name">
-      <p style="margin: 0px">{{ forecast.name }}<br />{{ forecast.temperature }}</p>
-      <img height="50" width="50" :src="forecast.icon" style="border-radius: 5px" />
+    <header>
+      <h1>{{ detailedForecast }}</h1>
+      <p width="400" style="fontSize: 100px; margin:10px">
+        {{ currentData.low }} / {{ currentData.high }}
+      </p>
+      <i class="wi" :class="currentData.icon" />
+    </header>
+    <div
+      style="display: inline-block; margin: 5px; width: 100px"
+      v-for="forecast in dailyForecasts"
+      :key="forecast.name"
+    >
+      <p style="margin: 0px">
+        {{ forecast.name }}
+        <br />
+        {{ forecast.low }}/{{ forecast.temperature }}
+      </p>
+      <i class="wi" :class="forecast.icon" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import _ from "lodash";
 import { setInterval } from "timers";
 
 const forecastURL = "https://api.weather.gov/gridpoints/BOX/66,74/forecast";
@@ -21,30 +35,103 @@ export default {
   name: "Forecast",
   data() {
     return {
-      currentTemp: 0,
+      currentData: {
+        high: 0,
+        icon: "",
+        low: 0
+      },
       detailedForecast: "",
-      dailyForecasts: []
+      dailyForecasts: [],
+      dummy: []
     };
   },
   created() {
-    axios.get(forecastHourlyURL).then(response => {
-      this.currentTemp = response.data.properties.periods[0].temperature;
-    });
+    axios.get(forecastHourlyURL).then(this.processHourly);
     setInterval(() => {
-      axios.get(forecastHourlyURL).then(response => {
-        this.currentTemp = response.data.properties.periods[0].temperature;
-      });
+      axios.get(forecastHourlyURL).then(this.processHourly);
     }, 3600000);
-    axios.get(forecastURL).then(response => {
-      this.dailyForecasts = response.data.properties.periods;
-      this.detailedForecast = response.data.properties.periods[0].detailedForecast;
-    });
+    axios.get(forecastURL).then(this.processDaily);
     setInterval(() => {
-      axios.get(forecastURL).then(response => {
-        this.dailyForecasts = response.data.properties.periods;
-        this.detailedForecast = response.data.properties.periods[0].detailedForecast;
-      });
+      axios.get(forecastURL).then(this.processDaily);
     }, 1200000);
+  },
+  methods: {
+    processHourly(response) {
+      this.currentData.high = response.data.properties.periods[0].temperature;
+      this.currentData.icon = this.convertIcon(
+        response.data.properties.periods[0].icon
+      );
+    },
+    processDaily(response) {
+      this.currentData.low = response.data.properties.periods[0].isDaytime
+        ? response.data.properties.periods[1].temperature
+        : response.data.properties.periods[0].temperature;
+      this.dailyForecasts = _.chain(response.data.properties.periods)
+        .dropWhile(item => item.number === 1 || item.name === "Tonight")
+        .chunk(2)
+        .map(item => _.set(item[0], "low", item[1].temperature))
+        .map(item => _.set(item, "icon", this.convertIcon(item.icon)))
+        .value();
+      this.detailedForecast =
+        response.data.properties.periods[0].detailedForecast;
+    },
+    convertIcon(icon) {
+      if (icon.includes("tsra")) {
+        return "wi-thunderstorm";
+      }
+      if (
+        icon.includes("sleet") ||
+        icon.includes("fzra") ||
+        icon.includes("rain_snow")
+      ) {
+        return "wi-rain-mix";
+      }
+      if (
+        icon.includes("skc") ||
+        icon.includes("few") ||
+        icon.includes("hot") ||
+        icon.includes("cold")
+      ) {
+        return "wi-day-sunny";
+      }
+      if (
+        icon.includes("sct") ||
+        icon.includes("bkn") ||
+        icon.includes("ovc")
+      ) {
+        return "wi-cloudy";
+      }
+      if (icon.includes("snow") || icon.includes("blizzard")) {
+        return "wi-snow";
+      }
+      if (icon.includes("rain")) {
+        return "wi-rain";
+      }
+      if (icon.includes("hurricane") || icon.includes("tropical_storm")) {
+        return "wi-hurricane";
+      }
+      if (
+        icon.includes("haze") ||
+        icon.includes("smoke") ||
+        icon.includes("dust")
+      ) {
+        return "wi-smog";
+      }
+      if (icon.includes("fog")) {
+        return "wi-fog";
+      }
+      if (icon.includes("tornado")) {
+        return "wi-tornado";
+      }
+      return "wi-na";
+    }
   }
 };
 </script>
+
+<style scoped>
+i {
+  color: grey;
+  font-size: 50px;
+}
+</style>
