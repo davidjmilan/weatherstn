ccdc2bccfe0b77992bfb2f2f8df239edb48e1771
David Milan <djmilan1@gmail.com>
2019-06-07 14:48:07 -0400

    remove vue-keyframe dependency

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from "vue";
-import { Keyframes } from "vue-keyframes";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-
-Vue.use(Keyframes);
 
 new Vue({
   render: h => h(App)
